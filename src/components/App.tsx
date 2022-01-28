5651afdecf192db43ffc71cfbf7c469ceba22383
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2022-01-29 00:11:24 +0200

    add elements improve style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,13 @@
+import { Form } from "./Form";
+
 export const App: React.FC = () => {
   return (
-    <div className="App">test</div>
+    <div className="App">
+      <div className="PromoText">
+        <h1>Learn to code by<br />watching others</h1>
+        <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </div>
+      <Form />
+    </div>
   )
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,7 @@
+export const Form: React.FC = () => {
+  return (
+    <div className="Form">
+      <button className="TryFreeButton"><strong>Try it free 7 days</strong> then<br />$20/mo. thereafter</button>
+    </div>
+  )
+}
