5c07d1b9418acca4e0b631a1f665d0c0ecc13a1c
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2022-01-30 16:10:33 +0200

    complete form button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -23,13 +23,17 @@ export const Form: React.FC = () => {
 
   return (
     <div className="FormContainer">
-      <button className="TryFreeButton"><strong>Try it free 7 days</strong> then<br />$20/mo. thereafter</button>
+      <div className="TryFreePanel"><div><strong>Try it free 7 days</strong> then<br />$20/mo. thereafter</div></div>
       <form>
         {Object.entries(userInfo).map(([key, value], index) => {
           return <Input key={index} name={key} placeholder={UserInfo[key as keyof typeof UserInfo]} type={key === "password" ? "password" : "text"} onUserChange={handleUserInfo} value={value} />
         })}
 
-        <button type="submit"></button>
+        <button type="button">CLAIM YOUR FREE TRIAL</button>
+
+        <div>
+          <p>By clicking the button, you are agreeing to our <a href="https://google.com" rel="noreferrer" target="_blank">Terns and Services</a></p>
+        </div>
       </form>
     </div>
   )
