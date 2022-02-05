a909c1af54b51d6348e54154a0b539e9ad8f5a60
serge-st <sergejs.stecenko@gmail.com>
2022-02-05 17:40:05 +0200

    small improvements

--- TODO.md
+++ TODO.md
@@ -1,3 +1,1 @@
-1. Complete Tablet styles
 2. Implement error handling
-3. Add styles to Form submitted div output
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -23,7 +23,9 @@ export const App: React.FC = () => {
         <h1>Learn to code by<br />watching others</h1>
         <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
       </div>
-      {isFormSubmitted ? <div>Data sent to your backend:<br /><pre>{`\n ${displayUserInfoExample()}`}</pre></div> : <Form changeAppState={changeAppState} />}
+      {isFormSubmitted ?
+        <div className="DemoPanel">Data sent to your backend:<br /><pre>{`\n ${displayUserInfoExample()}`}</pre></div> :
+        <Form changeAppState={changeAppState} />}
     </div>
   )
 }
