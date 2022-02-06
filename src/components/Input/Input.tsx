ffc7cc828e38c0bac8c9c9ae09077b8230d280b9
serge-st <sergejs.stecenko@gmail.com>
2022-02-06 08:32:42 +0200

    small input state improvement

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,20 +11,16 @@ interface InputProps {
 }
 
 export const Input: React.FC<InputProps> = ({ name, placeholder, type, onUserChange, value }) => {
-  const [isError, setIsError] = useState(false);
+  const [isError, setIsError] = useState(true);
   const [isFocused, setIsFocused] = useState(false);
 
   const handleFocus = () => {
-    setIsFocused(true);
-  }
-
-  const handleBlur = () => {
-    setIsFocused(false);
+    setIsFocused(!isFocused);
   }
 
   return (
     <div style={(isError && !isFocused) ? {marginBottom: ".3em"} : undefined} className={isError && !isFocused ? "errorExclamation" : ""}>
-      <input className={isError && !isFocused ? "errorInput" : ""} name={name} placeholder={placeholder} type={type} onChange={onUserChange} value={value} onFocus={handleFocus} onBlur={handleBlur} />
+      <input className={isError && !isFocused ? "errorInput" : ""} name={name} placeholder={placeholder} type={type} onChange={onUserChange} value={value} onFocus={handleFocus} onBlur={handleFocus} />
       {(isError && !isFocused) && <p className="errorMessage" >{UserInfoEnum[name as keyof typeof UserInfoEnum]} cannot be empty</p>}
     </div>
   )
